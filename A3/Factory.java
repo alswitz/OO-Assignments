c096e4d18373e0fb324c6cff4d43dfff5857e24a
alswitz <alexanderswitz09@gmail.com>
2017-04-03 22:54:37 -0400

    Changed some files.

--- A3/Factory.java
+++ A3/Factory.java
@@ -5,13 +5,10 @@ public class Factory
 	public static Factory getInstance()
 	{
 		if (m_instance == null)
-		{
 			m_instance = new Factory();
-		}
 		return m_instance;
 	}
 	
 	private Factory(){ }
-	
 	public IMessageParser createParser(String text){ return new MessageParser(text); }
 }
--- A3/WordReverse.java
+++ A3/WordReverse.java
@@ -5,23 +5,35 @@ public class WordReverse implements ICrypto
 	public static ICrypto getInstance()
 	{
 		if (m_instance == null)
-		{
 			m_instance = new WordReverse();
-		}
 		return m_instance;
 	}
 	
 	private WordReverse(){ }
 
 	@Override
-	public String encrypt(String message) {
-		// TODO Auto-generated method stub
-		return null;
+	public String encrypt(String message) 
+	{
+		IMessageParser msgIn = Factory.getInstance().createParser(message);
+		StringBuffer returnStr = new StringBuffer();
+		
+		for (int i = 0; i < msgIn.getWordCount(); i++)
+		{
+			String word = msgIn.getWord(i);
+			word = reverse(word);
+			returnStr.append(word + ' ');
+		}
+		return returnStr.toString().trim();
+	}
+	
+	private String reverse(String s)
+	{
+		StringBuffer returnStr = new StringBuffer();
+		for(int c = s.length() - 1; c >= 0; c--)
+			returnStr.append(s.charAt(c));
+		return returnStr.toString();
 	}
 
 	@Override
 	public String decrypt(String message){ return encrypt(message); }
-	
-	
-	
 }
